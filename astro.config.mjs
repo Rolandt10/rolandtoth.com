d8e52e5b24dda902e7f60820b9ad9d289299a038
Roland Toth <troland703@gmail.com>
2025-02-17 10:54:04 +0100

    Added plugin for calculating reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: "server",
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
