9f76f844f280d6f12c5f09c070ce203338692262
Roland Toth <troland703@gmail.com>
2025-02-24 17:47:53 +0100

    Responsive image wrapping

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -15,11 +15,11 @@ export default function Gallery() {
     <LightGallery plugins={[lgZoom]} mode="lg-fade">
       <div
         id="lightgallery"
-        className="flex w-full max-w-[1600px] flex-wrap justify-center gap-6"
+        className="mx-auto flex w-full max-w-[1200px] flex-wrap justify-center gap-6"
       >
         <a
           data-lg-size="1406-1390"
-          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 max-sm:h-60 max-sm:w-60 md:h-56 md:w-56 dark:border-dark-500/50 dark:bg-dark-700/60"
+          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 max-sm:aspect-square max-sm:h-auto max-sm:min-w-[130px] max-sm:max-w-[calc(50%-1rem)] max-sm:flex-1 max-sm:p-5 dark:border-dark-500/50 dark:bg-dark-700/60"
           data-src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80"
           data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
         >
@@ -33,7 +33,7 @@ export default function Gallery() {
           data-lg-size="1400-1400"
           data-pinterest-text="Shinimamiya, Osaka, Japan"
           data-tweet-text="Shinimamiya, Osaka, Japan"
-          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 sm:h-60 sm:w-60 md:h-56 md:w-56 dark:border-dark-500/50 dark:bg-dark-700/60"
+          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 max-sm:aspect-square max-sm:h-auto max-sm:min-w-[130px] max-sm:max-w-[calc(50%-1rem)] max-sm:flex-1 max-sm:p-5 dark:border-dark-500/50 dark:bg-dark-700/60"
           data-src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
           data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@asoshiation' >Shah </a></h4><p> Location - <a href='https://unsplash.com/s/photos/shinimamiya%2C-osaka%2C-japan'>Shinimamiya, Osaka, Japan</a></p>"
         >
@@ -45,7 +45,7 @@ export default function Gallery() {
 
         <a
           data-lg-size="1400-1400"
-          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 sm:h-60 sm:w-60 md:h-56 md:w-56 dark:border-dark-500/50 dark:bg-dark-700/60"
+          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 max-sm:aspect-square max-sm:h-auto max-sm:min-w-[130px] max-sm:max-w-[calc(50%-1rem)] max-sm:flex-1 max-sm:p-5 dark:border-dark-500/50 dark:bg-dark-700/60"
           data-src="https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
           data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
         >
@@ -57,7 +57,7 @@ export default function Gallery() {
 
         <a
           data-lg-size="1400-1400"
-          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 sm:h-60 sm:w-60 md:h-56 md:w-56 dark:border-dark-500/50 dark:bg-dark-700/60"
+          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 max-sm:aspect-square max-sm:h-auto max-sm:min-w-[130px] max-sm:max-w-[calc(50%-1rem)] max-sm:flex-1 max-sm:p-5 dark:border-dark-500/50 dark:bg-dark-700/60"
           data-iframe="true"
           data-src="https://www.lightgalleryjs.com/pdf/sample.pdf"
         >
@@ -68,7 +68,7 @@ export default function Gallery() {
         </a>
 
         <a
-          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 sm:h-60 sm:w-60 md:h-56 md:w-56 dark:border-dark-500/50 dark:bg-dark-700/60"
+          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 max-sm:aspect-square max-sm:h-auto max-sm:min-w-[130px] max-sm:max-w-[calc(50%-1rem)] max-sm:flex-1 max-sm:p-5 dark:border-dark-500/50 dark:bg-dark-700/60"
           data-src="https://www.youtube.com/watch?v=egyIeygdS_E&mute=0"
           key="4"
         >
@@ -81,7 +81,7 @@ export default function Gallery() {
 
         <a
           data-lg-size="1406-1390"
-          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 sm:h-60 sm:w-60 md:h-56 md:w-56 dark:border-dark-500/50 dark:bg-dark-700/60"
+          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 max-sm:aspect-square max-sm:h-auto max-sm:min-w-[130px] max-sm:max-w-[calc(50%-1rem)] max-sm:flex-1 max-sm:p-5 dark:border-dark-500/50 dark:bg-dark-700/60"
           data-src="https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80"
           data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
         >
@@ -95,7 +95,7 @@ export default function Gallery() {
           data-lg-size="1400-1400"
           data-pinterest-text="Shinimamiya, Osaka, Japan"
           data-tweet-text="Shinimamiya, Osaka, Japan"
-          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 sm:h-60 sm:w-60 md:h-56 md:w-56 dark:border-dark-500/50 dark:bg-dark-700/60"
+          className="gallery-item hover:border-zinc-700 flex h-52 w-52 cursor-zoom-in rounded-lg border border-solid p-7 transition duration-300 max-sm:aspect-square max-sm:h-auto max-sm:min-w-[130px] max-sm:max-w-[calc(50%-1rem)] max-sm:flex-1 max-sm:p-5 dark:border-dark-500/50 dark:bg-dark-700/60"
           data-src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
           data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@asoshiation' >Shah </a></h4><p> Location - <a href='https://unsplash.com/s/photos/shinimamiya%2C-osaka%2C-japan'>Shinimamiya, Osaka, Japan</a></p>"
         >
