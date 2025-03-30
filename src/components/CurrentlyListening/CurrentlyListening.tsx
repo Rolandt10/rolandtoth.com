d102242eb0c3803d1794d2da69e44ae11ff360c9
Roland Toth <troland703@gmail.com>
2025-03-30 17:30:48 +0200

    light mode

--- src/components/CurrentlyListening/CurrentlyListening.tsx
+++ src/components/CurrentlyListening/CurrentlyListening.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { MusicIcon, PauseIcon, PlayIcon } from "../Icons/Icons.tsx";
 
 const API_URL =
-  "https://adovd1cm2g.execute-api.us-east-1.amazonaws.com/production/currently-playing";
+  "https://za0j0wwqi3.execute-api.us-east-1.amazonaws.com/prod/currently-listening";
 
 function timeAgo(timestamp) {
   const now = new Date();
@@ -41,10 +41,17 @@ export default function CurrentSong() {
   const [song, setSong] = useState(null);
   const [error, setError] = useState(null);
 
+  const [dots, setDots] = useState("");
+
   useEffect(() => {
     const fetchSong = async () => {
       try {
-        const response = await fetch(API_URL);
+        const response = await fetch(API_URL, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
         if (!response.ok) throw new Error("Failed to fetch song");
         const data = await response.json();
         setSong(data);
@@ -55,7 +62,15 @@ export default function CurrentSong() {
     };
 
     fetchSong();
-    const interval = setInterval(fetchSong, 10000);
+    const interval = setInterval(fetchSong, 7500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev === "..." ? "." : prev + "."));
+    }, 1000);
 
     return () => clearInterval(interval);
   }, []);
@@ -84,29 +99,38 @@ export default function CurrentSong() {
     <>
       <a href={song.external_url} className="flex gap-4">
         <img
-          className="h-[4.1343rem] w-[4.1343rem] rounded-md object-cover"
+          className="h-[4.1343rem] w-[4.1343rem] rounded-md object-cover shadow-md"
           src={song.album_cover}
           alt=""
         />
         <div className="flex flex-col gap-1">
           <p className="text-base leading-none">{song.song}</p>
-          <p className="text-wrap text-sm dark:text-dark-300">
+          <p className="text-wrap text-sm text-light-400 dark:text-dark-300">
             {song.artists.join(", ")}
           </p>
-          <div className="flex w-max items-center gap-1 rounded-full bg-dark-600 pr-2 text-[0.78rem] text-dark-200">
-            <div className="flex items-center justify-center rounded-full border-[1px] border-dark-400/50 bg-dark-500 p-1">
+          <div
+            className={`${song.is_playing ? "w-[7.375rem]" : "w-max"} flex items-center gap-1 rounded-full bg-light-300/30 pr-2 text-[0.78rem] text-light-400 dark:bg-dark-600 dark:text-dark-200`}
+          >
+            <div className="flex items-center justify-center rounded-full border border-light-400/30 bg-light-300/20 p-1 dark:border-dark-400/50 dark:bg-dark-500">
               {song.is_playing ? (
                 <PlayIcon
                   size={12}
-                  className="relative right-[-1px] text-[0.8rem] text-dark-200"
+                  className="relative right-[-1px] text-[0.8rem] dark:text-dark-200"
                 />
               ) : (
-                <PauseIcon size={12} className="text-[0.8rem text-dark-200" />
+                <PauseIcon
+                  size={12}
+                  className="text-[0.8rem] text-light-400 dark:text-dark-200"
+                />
               )}
             </div>
-            {song.is_playing
-              ? "Now playing..."
-              : timeAgo(song.last_played_timestamp)}
+            {song.is_playing ? (
+              <div>
+                Now playing<span>{dots}</span>
+              </div>
+            ) : (
+              timeAgo(song.last_played_timestamp)
+            )}
           </div>
         </div>
       </a>
