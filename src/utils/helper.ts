de780833af1d7f7ae1604e21437a1434fc980021
Roland Toth <troland703@gmail.com>
2025-06-16 00:14:45 +0200

    fix(currentTrack): removed seconds

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -6,14 +6,9 @@ export function timeAgo(timestamp: number): string {
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
   const months = Math.floor(days / 30);
-  const years = Math.floor(days / 365);
-
-  if (seconds < 60) {
-    return seconds === 1 ? "a second ago" : `${seconds} seconds ago`;
-  }
 
   if (minutes < 60) {
-    return minutes === 1 ? "a minute ago" : `${minutes} minutes ago`;
+    return minutes <= 1 ? "a minute ago" : `${minutes} minutes ago`;
   }
 
   if (hours < 24) {
@@ -27,6 +22,4 @@ export function timeAgo(timestamp: number): string {
   if (months < 12) {
     return months === 1 ? "a month ago" : `${months} months ago`;
   }
-
-  return years === 1 ? "a year ago" : `${years} years ago`;
 }
