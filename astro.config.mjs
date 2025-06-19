432eba2056d0160a9da439a01ccbe05d4e4e3088
Roland Toth <troland703@gmail.com>
2025-06-19 11:28:05 +0200

    feat: added remark plugins to create anchor tags from headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
-import auth from "auth-astro";
+import remarkSlug from "remark-slug";
+import remarkAutolinkHeadings from "remark-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rolandtoth.com" || "localhost:4321",
   integrations: [tailwind(), react(), mdx()],
-  markdown: { remarkPlugins: [remarkReadingTime] },
+  markdown: {
+    remarkPlugins: [
+      remarkReadingTime,
+      remarkSlug,
+      [remarkAutolinkHeadings, { behavior: "wrap" }],
+    ],
+  },
 });
