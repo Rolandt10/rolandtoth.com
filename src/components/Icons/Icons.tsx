5761e130c22e8519a3f042633e68597bd5ee397b
Roland Toth <troland703@gmail.com>
2025-07-01 12:12:00 +0200

    fix(Icons): changed svg attributes to camelcase

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -8,9 +8,9 @@ const BaseIcon = ({ label = "", size = "20", children, ...props }) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       {children}
