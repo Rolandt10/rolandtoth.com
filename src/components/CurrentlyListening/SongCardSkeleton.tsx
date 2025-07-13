b40567b28ffeb4e92a21a4c1aa29942f4f72bbe5
Roland Toth <troland703@gmail.com>
2025-07-14 00:02:46 +0200

    fix: skeleton loader doesn't take up full width anymore

--- src/components/CurrentlyListening/SongCardSkeleton.tsx
+++ src/components/CurrentlyListening/SongCardSkeleton.tsx
@@ -1,6 +1,6 @@
 export default function SongCardSkeleton() {
   return (
-    <div className="flex w-full animate-pulse gap-4">
+    <div className="flex animate-pulse gap-4">
       <div className="h-[4.1343rem] w-[4.1343rem] rounded-md bg-dark-500 shadow-md" />
       <div className="flex flex-1 flex-col justify-between">
         <div className="h-5 w-3/4 rounded bg-dark-500" />
