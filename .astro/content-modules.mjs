a7f8c6723853b9c608c593df94b4b97d93208efe
Roland Toth <troland703@gmail.com>
2025-07-14 09:45:27 +0200

    chore: ignore .astro generated files

--- .astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/posts/post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fpost-1.mdx&astroContentModuleFlag=true")]]);
-		
